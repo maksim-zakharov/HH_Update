290f492a51bfa4c920937e07b4bdf9eeadb4d596
Максим <Max89701@gmail.com>
2016-01-01 03:28:14 +0300

    Загрузка

---
+++ HH_Update/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace HH_Update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                this.notifyIcon1.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "hh.ru";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Enabled = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ HH_Update/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Threading;
+
+namespace HH_Update
+{
+    public partial class Form1 : Form
+    {
+        HeadHunter hh;
+        const string name = "HeadHunter";
+        private ContextMenu m_menu; 
+        public Form1()
+        {
+            InitializeComponent();
+            m_menu = new ContextMenu();
+            m_menu.MenuItems.Add(new MenuItem("Exit", new System.EventHandler(Exit_Click)));
+            notifyIcon1.ContextMenu = m_menu;   
+            timer1.Interval = (int)new TimeSpan(0,1,0).TotalMilliseconds;
+            SetAutorunValue(true);
+            notifyIcon1.Icon = Properties.Resources.hh;
+            notifyIcon1.Visible = true;
+            hh = new HeadHunter();
+            Auth();
+        }
+        public void Auth()
+        {
+            try
+            {
+                hh.Authentication("Login","Pass");
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(30000);
+                Auth();
+            }
+        }
+        protected void Exit_Click(Object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                await hh.UpdateAllAsync();
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+        public bool SetAutorunValue(bool autorun)
+        {
+            string ExePath = System.Windows.Forms.Application.ExecutablePath;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                    reg.SetValue(name, ExePath);
+                else
+                    reg.DeleteValue(name);
+
+                reg.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            Hide();
+        }
+    }
+}
